626f308c33c356ad5446181652ffe6f8fa03b7d3
jenkins dev <jenkins dev>
2019-10-14 13:35:32 +0800

    fix dns shell

--- alicloud/scripts/import/dns.sh
+++ alicloud/scripts/import/dns.sh
@@ -2,35 +2,21 @@
 function TerraformImport() {
     domain_replace=$(echo ${domain}|sed 's/\./_/g')
     echo terraform import module.dns-${domain_replace}.module.domain.alicloud_dns.this[0] ${domain}
-#    terraform import module.dns-${domain_replace}.module.domain.alicloud_dns.this[0] ${domain}
+    terraform import module.dns-${domain_replace}.module.domain.alicloud_dns.this[0] ${domain}
     
-    echo "record_list=[" > ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
     cat /dev/null > ${temp_domain_file}
     for((i=1;i<=2;i++));
     do
-        aliyun alidns DescribeDomainRecords --PageSize=500 --PageNumber ${i} --DomainName=${domain} | jq -r '.DomainRecords.Record' |jq .[].RecordId | sed 's/"//g' >> ${temp_domain_file}
-#        lists=$(aliyun alidns DescribeDomainRecords --PageSize=500 --PageNumber ${j} --DomainName=${domain} | jq -r '.DomainRecords.Record' |jq .[].RecordId | sed 's/"//g')
+        aliyun alidns DescribeDomainRecords --PageSize=500 --PageNumber ${i} --DomainName=${domain} | jq -r '.DomainRecords.Record' >> ${temp_domain_file}
     done
-    lists=$(cat ${temp_domain_file})
+    lists=$(cat ${temp_domain_file} | jq .[].RecordId | sed 's/"//g' )
     j=0
     for list in $lists
     do
         echo terraform import module.dns-${domain_replace}.module.records.alicloud_dns_record.this[${j}] ${list}
         terraform import module.dns-${domain_replace}.module.records.alicloud_dns_record.this[${j}] ${list}
-        echo terraform state show -no-color module.dns-${domain_replace}.module.records.alicloud_dns_record.this[${j}] >> ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
-        terraform state show -no-color module.dns-${domain_replace}.module.records.alicloud_dns_record.this[${j}] >> ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
         j=$(($j+1))
     done
-    #terraform show -no-color |grep -A12 module.dns-${domain_replace}.module.records.alicloud_dns_record.this >> ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
-    #sed -i '/--/d' ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
-    sed -i '/#/d' ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
-    sed -i '/id          =/d' ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
-    sed -i '/locked      =/d' ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
-    sed -i '/name        =/d' ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
-    sed -i '/status      =/d' ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
-    sed -i 's/}/},/g' ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
-    sed -i 's/.*{/{/g' ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
-    sed -i '$a]' ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
 }
 
 basepath=$(cd $(dirname $0); pwd)
@@ -42,3 +28,13 @@ do
     temp_domain_file=/tmp/dns-${domain}
     TerraformImport ${domain}
 done
+
+echo -n "record_list=" > ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
+sed -i 's/\"RR\":/host_record=/g' ${temp_domain_file}
+sed -i 's/\"Type\":/type=/g'  ${temp_domain_file}
+sed -i 's/\"Value\":/value=/g'  ${temp_domain_file}
+sed -i 's/\"Line\":/routing=/g'  ${temp_domain_file}
+sed -i 's/\"TTL\":/ttl=/g'  ${temp_domain_file}
+sed -i '/host_record/a\\    priority    = 0'  ${temp_domain_file}
+sed -i '/\[\]/d'  ${temp_domain_file}
+cat  ${temp_domain_file} | grep -v '^    \"' >> ${VARIABLES_PATH}/dns/${domain_replace}.tfvars
